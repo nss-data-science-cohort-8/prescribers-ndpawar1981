a0135f94ec8941fe7068bad7b5ffd3bc329576e5
ndpawar1981 <nitin1pawar@gmail.com>
2025-02-26 05:50:03 -0600

    finished upto 5a

--- nitin_prescriber.sql
+++ nitin_prescriber.sql
@@ -102,6 +102,80 @@ FROM
 GROUP BY
 	D.GENERIC_NAME
 ORDER BY
-	DRUG_COST DESC;
+	DRUG_COST DESC
+	LIMIT 1;
 
 	--b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.**
+
+SELECT
+	D.GENERIC_NAME AS DRUG_GENERIC_NAME,
+	MAX(
+		ROUND(PR.TOTAL_DRUG_COST / PR.TOTAL_DAY_SUPPLY, 2)
+	) AS DRUG_COST_PER_DAY
+FROM
+	DRUG D
+	INNER JOIN PRESCRIPTION PR ON PR.DRUG_NAME = D.DRUG_NAME
+GROUP BY
+	D.GENERIC_NAME
+ORDER BY
+	DRUG_COST_PER_DAY DESC LIMIT
+	1;
+
+--4. 
+--    a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', 
+-- says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs. **Hint:** You may want to use a CASE expression for this. 
+--See https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-case/ 
+
+SELECT
+	DRUG_NAME,
+	CASE
+		WHEN OPIOID_DRUG_FLAG = 'Y' THEN 'OPIOID'
+		WHEN ANTIBIOTIC_DRUG_FLAG = 'Y' THEN 'ANTIBIOTIC'
+		ELSE 'NEITHER'
+	END AS DRUG_TYPE FROM
+	DRUG;
+
+--b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. 
+--Hint: Format the total costs as MONEY for easier comparision.
+
+SELECT
+	DRUG2.DRUG_TYPE AS DRUG_TYPE,
+	cast(SUM(PR.TOTAL_DRUG_COST) as money) AS DRUG_COST 
+FROM
+	PRESCRIPTION PR 
+	INNER JOIN (
+		SELECT
+			DRUG_NAME,
+			CASE
+				WHEN OPIOID_DRUG_FLAG = 'Y' THEN 'OPIOID'
+				WHEN ANTIBIOTIC_DRUG_FLAG = 'Y' THEN 'ANTIBIOTIC'
+				ELSE 'NEITHER'
+			END AS DRUG_TYPE
+		FROM
+			DRUG
+	) AS DRUG2 ON DRUG2.DRUG_NAME = PR.DRUG_NAME
+WHERE
+	DRUG2.DRUG_TYPE IN ('OPIOID', 'ANTIBIOTIC')
+GROUP BY
+	DRUG2.DRUG_TYPE
+ORDER BY	DRUG_COST DESC;
+
+
+--5. 
+    --a. How many CBSAs are in Tennessee? **Warning:** The cbsa table contains information for all states, not just Tennessee.
+
+SELECT
+	COUNT(distinct CBSA) AS TOTAL_CBSA
+FROM
+	CBSA
+WHERE
+	FIPSCOUNTY IN (
+		SELECT DISTINCT
+			FIPSCOUNTY
+		FROM
+			FIPS_COUNTY
+		WHERE
+			STATE = 'TN'
+	);
+
+--b. Which cbsa has the largest combined population? Which has the smallest? Report the CBSA name and total population.
